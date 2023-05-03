6e1bf66e57b7cdd522e9be1bf7ebe64858bcc9f2
kero1976 <kero1976@outlook.jp>
2023-05-03 16:21:43 +0900

    ロガーを追加

--- javatool/downloader/src/main/java/downloader/Sample1.java
+++ javatool/downloader/src/main/java/downloader/Sample1.java
@@ -1,5 +1,8 @@
 package downloader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * これはUTF-8です。
  * @author kero
@@ -7,11 +10,13 @@ package downloader;
  */
 public class Sample1 {
 
+    private static Logger log = LoggerFactory.getLogger(Sample1.class);
 
     public int add(int a, int b) {
+        log.debug("START:a={}, b={}", a, b);
         return a + b;
     }
-    
+
     public int double2(int a) {
         return a *2;
     }
